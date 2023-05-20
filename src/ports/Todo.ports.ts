358782ee4de86e731764266ff0d5de3cef490b0b
SofianeT <89205501+SofianeT@users.noreply.github.com>
2023-05-20 13:43:12 +0200

    Todo

--- src/ports/Todo.ports.ts
+++ src/ports/Todo.ports.ts
@@ -3,25 +3,25 @@ import axios from "axios/index";
 
 
 export default class TodoPorts {
-   // #url = "https://6423efc847401740432f0134.mockapi.io/api/v1";
+   // #url = "https://646332f37a9eead6fadfb61c.mockapi.io/";
 
     getAllTodo() {
-        const endPoint = "todo";
+        const endPoint = "Todo";
 
         return axios.get(`${this.#url}/${endPoint}`);
     }
 
     createTodo(todo: Todo) {
-        const endpoint = "todo";
+        const endpoint = "Todo";
         return axios.post(`${this.#url}/${endpoint}`, todo);
     }
 
     updateTodo(todo: Todo) {
-        const endpoint = "todo";
+        const endpoint = "Todo";
         return axios.put(`${this.#url}/${endpoint}/${todo.id}`, todo);
     }
     deleteTodo(idTodo: string) {
-        const endpoint = "todo";
+        const endpoint = "Todo";
         return axios.delete(`${this.#url}/${endpoint}/${idTodo}`);
     }
 }
