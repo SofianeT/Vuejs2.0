662523980d42cd042f44649b43462823411f685e
SofianeT <89205501+SofianeT@users.noreply.github.com>
2023-05-21 18:09:18 +0200

    url

--- src/ports/Todo.ports.ts
+++ src/ports/Todo.ports.ts
@@ -4,27 +4,26 @@ import axios from "axios";
 
 export default class TodoPorts {
 
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    #url = "https://646332f37a9eead6fadfb61c.mockapi.io/";
+
+    url = "https://646332f37a9eead6fadfb61c.mockapi.io/";
 
     getAllTodo() {
         const endPoint = "Todo";
 
-        return axios.get(`${this.#url}/${endPoint}`);
+        return axios.get(`${this.url}/${endPoint}`);
     }
 
     createTodo(todo: Todo) {
         const endpoint = "Todo";
-        return axios.post(`${this.#url}/${endpoint}`, todo);
+        return axios.post(`${this.url}/${endpoint}`, todo);
     }
 
     updateTodo(todo: Todo) {
         const endpoint = "Todo";
-        return axios.put(`${this.#url}/${endpoint}/${todo.id}`, todo);
+        return axios.put(`${this.url}/${endpoint}/${todo.id}`, todo);
     }
     deleteTodo(idTodo: string) {
         const endpoint = "Todo";
-        return axios.delete(`${this.#url}/${endpoint}/${idTodo}`);
+        return axios.delete(`${this.url}/${endpoint}/${idTodo}`);
     }
 }
